fffbcd6ec710082240c59e96275ea894db374b08
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-03 14:31:00 +0800

    feat starred repo page

---
+++ lib/starred_repos/presentation/starred_repo_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:repo_viewer/auth/shared/provider.dart';
+
+class StarredReposPage extends ConsumerWidget {
+  const StarredReposPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(authNotifierProvider.notifier).signOut();
+          },
+          child: const Text('Sign Out'),
+        ),
+      ),
+    );
+  }
+}
