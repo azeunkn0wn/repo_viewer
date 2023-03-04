71a7247b050a996119c609a763a31efeff71267a
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-04 15:23:54 +0800

    add github headers data class with parsing methods

---
+++ lib/github/infrastructure/github_headers.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'github_headers.freezed.dart';
+part 'github_headers.g.dart';
+
+@freezed
+class GithubHeaders with _$GithubHeaders {
+  const GithubHeaders._();
+  const factory GithubHeaders({
+    String? etag,
+    PaginationLink? link,
+  }) = _GithubHeaders;
+
+  factory GithubHeaders.parse(Response response) {
+    final link = response.headers.map['Link']?[0];
+
+    return GithubHeaders(
+      etag: response.headers.map['ETag']?.first,
+      link: link == null
+          ? null
+          : PaginationLink.parse(
+              link.split(', '),
+              requestUrl: response.requestOptions.uri.toString(),
+            ),
+    );
+  }
+
+  factory GithubHeaders.fromJson(Map<String, dynamic> json) =>
+      _$GithubHeadersFromJson(json);
+}
+
+@freezed
+class PaginationLink with _$PaginationLink {
+  const PaginationLink._();
+  const factory PaginationLink({
+    required int maxPage,
+  }) = _PaginationLink;
+
+  factory PaginationLink.parse(
+    List<String> values, {
+    required String requestUrl,
+  }) {
+    return PaginationLink(
+      maxPage: _extractPageNumber(
+        values.firstWhere(
+          (e) => e.contains('rel="last"'),
+          orElse: () => requestUrl,
+        ),
+      ),
+    );
+  }
+
+  static int _extractPageNumber(String value) {
+    final uriString = RegExp(
+      r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)',
+    ).stringMatch(value);
+
+    return int.parse(Uri.parse(uriString!).queryParameters['page']!);
+  }
+
+  factory PaginationLink.fromJson(Map<String, dynamic> json) =>
+      _$PaginationLinkFromJson(json);
+}
