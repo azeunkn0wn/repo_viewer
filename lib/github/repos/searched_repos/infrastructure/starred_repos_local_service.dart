7026c4e6b92208fbde0e6687127466419fa9b6fa
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-05 20:54:18 +0800

    add starred repo local service

---
+++ lib/github/repos/searched_repos/infrastructure/starred_repos_local_service.dart
@@ -0,0 +1,41 @@
+import 'package:collection/collection.dart';
+import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
+import 'package:repo_viewer/github/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/infrastructure/pagination_config.dart';
+import 'package:sembast/sembast.dart';
+
+class StarredReposLocalService {
+  final SembastDatabase _sembastDatabase;
+  final _store = intMapStoreFactory.store('starredRepos');
+
+  StarredReposLocalService(this._sembastDatabase);
+
+  Future<void> upsertPage(List<GithubRepoDTO> dtos, int page) async {
+    final sembastPage = page - 1;
+
+    _store
+        .records(
+          dtos.mapIndexed(
+            (index, _) => index + PaginationConfig.itemsPerPage * sembastPage,
+          ),
+        )
+        .put(
+          _sembastDatabase.instance,
+          dtos.map((e) => e.toJson()).toList(),
+        );
+  }
+
+  Future<List<GithubRepoDTO>> getPage(int page) async {
+    final sembastPage = page - 1;
+
+    final records = await _store.find(
+      _sembastDatabase.instance,
+      finder: Finder(
+        limit: PaginationConfig.itemsPerPage,
+        offset: PaginationConfig.itemsPerPage * sembastPage,
+      ),
+    );
+
+    return records.map((e) => GithubRepoDTO.fromJson(e.value)).toList();
+  }
+}
