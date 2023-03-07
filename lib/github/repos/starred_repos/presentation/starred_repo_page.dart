5adf81c89a1477fe2f1aa5052958cef465e56096
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-07 16:35:03 +0800

    update add list view and initialized states

--- lib/github/repos/starred_repos/presentation/starred_repo_page.dart
+++ lib/github/repos/starred_repos/presentation/starred_repo_page.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import 'package:repo_viewer/auth/shared/providers.dart';
+import 'package:repo_viewer/github/repos/starred_repos/presentation/paginated_repos_list_view.dart';
+import 'package:repo_viewer/github/shared/providers.dart';
 
-class StarredReposPage extends ConsumerWidget {
+class StarredReposPage extends ConsumerStatefulWidget {
   const StarredReposPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _StarredReposPageState();
+}
+
+class _StarredReposPageState extends ConsumerState<StarredReposPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => ref
+          .read(starredReposNotifierProvider.notifier)
+          .getNextStarredReposPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ref.read(authNotifierProvider.notifier).signOut();
-          },
-          child: const Text('Sign Out'),
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Starred Repos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(authNotifierProvider.notifier).signOut();
+            },
+            icon: const Icon(MdiIcons.logoutVariant),
+          ),
+        ],
       ),
+      body: const PaginatedReposListView(),
     );
   }
 }
