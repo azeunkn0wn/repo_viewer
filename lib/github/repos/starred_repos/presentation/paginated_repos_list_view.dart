24a18de8d0d5b56835bc1263d760cbf570bf56cf
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-07 16:42:16 +0800

    refactor extracted ListView widget to private

--- lib/github/repos/starred_repos/presentation/paginated_repos_list_view.dart
+++ lib/github/repos/starred_repos/presentation/paginated_repos_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/github/repos/starred_repos/application/starred_repos_notifier.dart';
 import 'package:repo_viewer/github/repos/starred_repos/presentation/failure_repo_tile.dart';
 import 'package:repo_viewer/github/repos/starred_repos/presentation/loading_repo_tile.dart';
 import 'package:repo_viewer/github/repos/starred_repos/presentation/repo_tile.dart';
@@ -15,36 +16,49 @@ class PaginatedReposListView extends StatelessWidget {
     return Consumer(
       builder: (context, ref, child) {
         final state = ref.watch(starredReposNotifierProvider);
-        return ListView.builder(
-          itemCount: state.map(
-            initial: (_) => 0,
-            loadInProgress: (_) => _.repos.entity.length + _.itemsPerPage,
-            loadSuccess: (_) => _.repos.entity.length,
-            loadFailure: (_) => _.repos.entity.length + 1,
+        return _PaginatedListVIew(state: state);
+      },
+    );
+  }
+}
+
+class _PaginatedListVIew extends StatelessWidget {
+  const _PaginatedListVIew({
+    required this.state,
+  });
+
+  final StarredReposState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: state.map(
+        initial: (_) => 0,
+        loadInProgress: (_) => _.repos.entity.length + _.itemsPerPage,
+        loadSuccess: (_) => _.repos.entity.length,
+        loadFailure: (_) => _.repos.entity.length + 1,
+      ),
+      itemBuilder: (context, index) {
+        return state.map(
+          initial: (_) => RepoTile(repo: _.repos.entity[index]),
+          loadInProgress: (_) {
+            if (index < _.repos.entity.length) {
+              return RepoTile(repo: _.repos.entity[index]);
+            } else {
+              return const LoadingRepoTile();
+            }
+          },
+          loadSuccess: (_) => RepoTile(
+            repo: _.repos.entity[index],
           ),
-          itemBuilder: (context, index) {
-            return state.map(
-              initial: (_) => RepoTile(repo: _.repos.entity[index]),
-              loadInProgress: (_) {
-                if (index < _.repos.entity.length) {
-                  return RepoTile(repo: _.repos.entity[index]);
-                } else {
-                  return const LoadingRepoTile();
-                }
-              },
-              loadSuccess: (_) => RepoTile(
-                repo: _.repos.entity[index],
-              ),
-              loadFailure: (_) {
-                if (index < _.repos.entity.length) {
-                  return RepoTile(repo: _.repos.entity[index]);
-                } else {
-                  return FailureRepoTile(
-                    failure: _.failure,
-                  );
-                }
-              },
-            );
+          loadFailure: (_) {
+            if (index < _.repos.entity.length) {
+              return RepoTile(repo: _.repos.entity[index]);
+            } else {
+              return FailureRepoTile(
+                failure: _.failure,
+              );
+            }
           },
         );
       },
