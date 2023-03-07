a3c13e54fd2340dcadaab3696864a3217b5827a4
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-07 16:37:52 +0800

    add repo tile when error occurred

---
+++ lib/github/repos/starred_repos/presentation/failure_repo_tile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:repo_viewer/github/core/domain/github_failure.dart';
+import 'package:repo_viewer/github/shared/providers.dart';
+
+class FailureRepoTile extends ConsumerWidget {
+  final GithubFailure failure;
+
+  const FailureRepoTile({
+    super.key,
+    required this.failure,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTileTheme(
+      textColor: Theme.of(context).colorScheme.onError,
+      iconColor: Theme.of(context).colorScheme.onError,
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        color: Theme.of(context).colorScheme.error,
+        child: ListTile(
+          leading: const SizedBox(
+            height: double.infinity,
+            child: Icon(
+              Icons.warning,
+            ),
+          ),
+          title: const Text('An error occurred. Please, retry'),
+          subtitle: Text(
+            failure.map(
+              api: (_) => 'Api returned ${_.errorCode}',
+            ),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: SizedBox(
+            height: double.infinity,
+            child: IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                ref
+                    .read(starredReposNotifierProvider.notifier)
+                    .getNextStarredReposPage();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
