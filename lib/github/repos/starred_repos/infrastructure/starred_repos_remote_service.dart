fda4058af2ef310e80c20431f4dbb68936c093d6
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-09 18:02:30 +0800

    refactor extracted common code to abstract class

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,72 +1,26 @@
-import 'package:dio/dio.dart';
-import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
-import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
 import 'package:repo_viewer/core/infrastructure/remote_response.dart';
-import 'package:repo_viewer/github/infrastructure/github_headers.dart';
-import 'package:repo_viewer/github/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/infrastructure/github_repo_dto.dart';
 import 'package:repo_viewer/github/infrastructure/pagination_config.dart';
+import 'package:repo_viewer/github/repos/core/infrastructure/repos_remote_service.dart';
 
-class StarredReposRemoteService {
-  final Dio _dio;
-  final GithubHeadersCache _headersCache;
-
+class StarredReposRemoteService extends ReposRemoteService {
   StarredReposRemoteService(
-    this._dio,
-    this._headersCache,
+    super.dio,
+    super.headersCache,
   );
 
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
     int page,
-  ) async {
-    final requestUri = Uri.https(
-      'api.github.com',
-      '/user/starred',
-      {
-        'page': page.toString(),
-        'per_page': PaginationConfig.itemsPerPage.toString()
-      },
-    );
-
-    final previousHeaders = await _headersCache.getHeaders(requestUri);
-
-    try {
-      final response = await _dio.getUri(
-        requestUri,
-        options: Options(
-          headers: {
-            'If-None-Match': previousHeaders?.etag ?? '',
+  ) async =>
+      super.getPage(
+        requestUri: Uri.https(
+          'api.github.com',
+          '/user/starred',
+          {
+            'page': '$page',
+            'per_page': PaginationConfig.itemsPerPage.toString()
           },
         ),
+        jsonDataSelector: (json) => json as List<dynamic>,
       );
-
-      if (response.statusCode == 304) {
-        return RemoteResponse.notModified(
-          maxPage: previousHeaders?.link?.maxPage ?? 0,
-        );
-      } else if (response.statusCode == 200) {
-        final headers = GithubHeaders.parse(response);
-        await _headersCache.saveHeaders(requestUri, headers);
-
-        final convertedData = (response.data as List<dynamic>)
-            .map((e) => GithubRepoDTO.fromJson(e as Map<String, dynamic>))
-            .toList();
-
-        return RemoteResponse.withNewData(
-          convertedData,
-          maxPage: headers.link?.maxPage ?? 0,
-        );
-      } else {
-        throw RestApiException(response.statusCode);
-      }
-    } on DioError catch (e) {
-      if (e.isNoConnectionError) {
-        return const RemoteResponse.noConnection();
-      } else if (e.response != null) {
-        throw RestApiException(e.response?.statusCode);
-      } else {
-        rethrow;
-      }
-    }
-  }
 }
