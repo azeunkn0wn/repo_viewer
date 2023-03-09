cb9348c16676d2fa743b7a1ec5f4889d99bb901b
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-09 18:04:19 +0800

    refactor extracted object extension to repo core

---
+++ lib/github/repos/core/infrastructure/extensions.dart
@@ -0,0 +1,8 @@
+import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/infrastructure/github_repo_dto.dart';
+
+extension DTOListToDomainList on List<GithubRepoDTO> {
+  List<GithubRepo> toDomain() {
+    return map((e) => e.toDomain()).toList();
+  }
+}
