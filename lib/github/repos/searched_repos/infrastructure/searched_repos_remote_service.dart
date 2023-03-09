0ef223a18f77313c24a3a74ff76c049971626a4c
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-09 18:04:41 +0800

    feat searched repo remote service

---
+++ lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
@@ -0,0 +1,31 @@
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/infrastructure/pagination_config.dart';
+import 'package:repo_viewer/github/repos/core/infrastructure/repos_remote_service.dart';
+
+class SearchedReposRemoteService extends ReposRemoteService {
+  SearchedReposRemoteService(
+    super.dio,
+    super.headersCache,
+  );
+
+  Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
+    String query,
+    int page,
+  ) async =>
+      super.getPage(
+        requestUri: Uri.https(
+          'api.github.com',
+          '/searched/repositories',
+          {
+            'q': query,
+            'page': '$page',
+            'per_page': PaginationConfig.itemsPerPage.toString()
+          },
+        ),
+        //? added Map<String,dynamic> on json to
+        //? to satisfy the "avoid method calls on dynamic warning"
+        jsonDataSelector: (json) =>
+            (json as Map<String, dynamic>)['items'] as List<dynamic>,
+      );
+}
