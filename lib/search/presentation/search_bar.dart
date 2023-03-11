b0fd621b3cfdcf905542654955b3d5fe81d2225e
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-11 20:59:20 +0800

    feat search bar widget

---
+++ lib/search/presentation/search_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+import 'package:repo_viewer/search/shared/providers.dart';
+
+class SearchBar extends ConsumerStatefulWidget {
+  final Widget body;
+  final String title;
+  final String hint;
+  final void Function(String searchTerm) onShouldNavigateToResultPage;
+  final void Function(String searchTerm) onSignOutButtonPressed;
+  const SearchBar({
+    required this.body,
+    required this.title,
+    required this.hint,
+    required this.onShouldNavigateToResultPage,
+    required this.onSignOutButtonPressed,
+  });
+  @override
+  ConsumerState<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends ConsumerState<SearchBar> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(searchHistoryNotifierProvider.notifier).watchSearchTerms();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingSearchBar(
+      body: widget.body,
+      title: Text(widget.title),
+      hint: widget.hint,
+      builder: (context, transition) {
+        return Container();
+      },
+    );
+  }
+}
