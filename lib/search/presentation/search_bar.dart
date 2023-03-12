efda5da3f9c2be13ffa7b0a4d4edde9680b3e910
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-12 16:05:47 +0800

    update search history and refactor duplicates

--- lib/search/presentation/search_bar.dart
+++ lib/search/presentation/search_bar.dart
@@ -39,6 +39,22 @@ class _SearchBarState extends ConsumerState<SearchBar> {
 
   @override
   Widget build(BuildContext context) {
+    void pushPageAndPutFirstInHistory(String searchTerm) {
+      widget.onShouldNavigateToResultPage(searchTerm);
+      ref
+          .read(searchHistoryNotifierProvider.notifier)
+          .putSearchTermFirst(searchTerm);
+      _controller.close();
+    }
+
+    void pushPageAndAddToHistory(String searchTerm) {
+      widget.onShouldNavigateToResultPage(searchTerm);
+      ref
+          .read(searchHistoryNotifierProvider.notifier)
+          .addSearchTerm(searchTerm);
+      _controller.close();
+    }
+
     return FloatingSearchBar(
       controller: _controller,
       body: FloatingSearchBarScrollNotifier(child: widget.body),
@@ -67,36 +83,80 @@ class _SearchBarState extends ConsumerState<SearchBar> {
           ),
         )
       ],
+      onQueryChanged: (query) {
+        ref
+            .read(searchHistoryNotifierProvider.notifier)
+            .watchSearchTerms(filter: query);
+      },
       onSubmitted: (query) {
-        widget.onShouldNavigateToResultPage(query);
-        ref.read(searchHistoryNotifierProvider.notifier).addSearchTerm(query);
-        _controller.close();
+        pushPageAndAddToHistory(query);
       },
       builder: (context, transition) {
-        return Consumer(
-          builder: (context, ref, child) {
-            final searchHistory = ref.watch(searchHistoryNotifierProvider);
-
-            return searchHistory.map(
-              data: (history) {
-                return Column(
-                  children: history.value
-                      .map(
-                        (term) => ListTile(
-                          title: Text(term),
-                        ),
-                      )
-                      .toList(),
-                );
-              },
-              loading: (_) => const ListTile(
-                title: LinearProgressIndicator(),
-              ),
-              error: (_) => ListTile(
-                title: Text('Very unexpected error ${_.error}'),
-              ),
-            );
-          },
+        return Material(
+          color: Theme.of(context).cardColor,
+          elevation: 4,
+          borderRadius: BorderRadius.circular(8),
+          clipBehavior: Clip.hardEdge,
+          child: Consumer(
+            builder: (context, ref, child) {
+              final searchHistory = ref.watch(searchHistoryNotifierProvider);
+              return searchHistory.map(
+                data: (history) {
+                  if (_controller.query.isEmpty && history.value.isEmpty) {
+                    return Container(
+                      height: 56,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Start searching',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    );
+                  } else if (history.value.isEmpty) {
+                    return ListTile(
+                      title: Text(_controller.query),
+                      leading: const Icon(Icons.search),
+                      onTap: () {
+                        pushPageAndAddToHistory(_controller.query);
+                      },
+                    );
+                  }
+                  return Column(
+                    children: history.value
+                        .map(
+                          (term) => ListTile(
+                            title: Text(
+                              term,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            leading: const Icon(Icons.history),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                ref
+                                    .read(
+                                      searchHistoryNotifierProvider.notifier,
+                                    )
+                                    .deleteSearchTerm(term);
+                              },
+                            ),
+                            onTap: () {
+                              pushPageAndPutFirstInHistory(term);
+                            },
+                          ),
+                        )
+                        .toList(),
+                  );
+                },
+                loading: (_) => const ListTile(
+                  title: LinearProgressIndicator(),
+                ),
+                error: (_) => ListTile(
+                  title: Text('Very unexpected error ${_.error}'),
+                ),
+              );
+            },
+          ),
         );
       },
     );
