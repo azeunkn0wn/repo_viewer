6e33a7f8b813b8194b0f107b3a8d5c6dd5b4c266
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-13 19:06:05 +0800

    add delete cached eTag for the deleted entry

--- lib/github/detail/infrastructure/repo_detail_local_service.dart
+++ lib/github/detail/infrastructure/repo_detail_local_service.dart
@@ -1,5 +1,6 @@
 import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
 import 'package:repo_viewer/github/detail/infrastructure/github_repo_detail_dto.dart';
+import 'package:repo_viewer/github/infrastructure/github_headers_cache.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/timestamp.dart';
 
@@ -8,9 +9,9 @@ class RepoDetailLocalService {
 
   final SembastDatabase _sembastDatabase;
   final _store = stringMapStoreFactory.store('repoDetails');
-  RepoDetailLocalService(
-    this._sembastDatabase,
-  );
+
+  final GithubHeadersCache _headersCache;
+  RepoDetailLocalService(this._sembastDatabase, this._headersCache);
 
   Future<void> upsertRepoDetail(GithubRepoDetailDTO githubRepoDetailDTO) async {
     await _store.record(githubRepoDetailDTO.fullName).put(
@@ -29,6 +30,12 @@ class RepoDetailLocalService {
 
       for (final key in keysToRemove) {
         await _store.record(key).delete(_sembastDatabase.instance);
+        await _headersCache.deleteHeaders(
+          Uri.https(
+            'api.github.com',
+            '/repos/$key/readme',
+          ),
+        );
       }
     }
   }
