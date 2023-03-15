581e57da3d3ca83d6ac4509276ebeb59fc440f53
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-15 15:46:08 +0800

    update to consumer widget and add star action btn

--- lib/github/detail/presentation/repo_detail_page.dart
+++ lib/github/detail/presentation/repo_detail_page.dart
@@ -1,19 +1,52 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:repo_viewer/core/presentation/toast.dart';
 import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/core/shared/providers.dart';
+import 'package:repo_viewer/github/detail/application/repo_detail_notifier.dart';
+import 'package:shimmer/shimmer.dart';
 
-class RepoDetailPage extends StatefulWidget {
+class RepoDetailPage extends ConsumerStatefulWidget {
   final GithubRepo repo;
   const RepoDetailPage({super.key, required this.repo});
 
   @override
-  State<RepoDetailPage> createState() => _RepoDetailPageState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _RepoDetailPageState();
 }
 
-class _RepoDetailPageState extends State<RepoDetailPage> {
+class _RepoDetailPageState extends ConsumerState<RepoDetailPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => ref
+          .read(repoDetailNotifierProvider.notifier)
+          .getRepoDetail(widget.repo.fullName),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final state = ref.watch(repoDetailNotifierProvider);
+    ref.listen<RepoDetailState>(
+      repoDetailNotifierProvider,
+      (_, state) {
+        state.maybeWhen(
+          orElse: () {},
+          loadSuccess: (repoDetail, _) {
+            if (!repoDetail.isFresh) {
+              showNoConnectionToast(
+                "You're offline. Some information may be outdated.",
+                context,
+              );
+            }
+          },
+        );
+      },
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -36,7 +69,39 @@ class _RepoDetailPageState extends State<RepoDetailPage> {
             )
           ],
         ),
+        actions: [
+          state.maybeMap(
+            orElse: () => Shimmer.fromColors(
+              baseColor: Colors.grey.shade400,
+              highlightColor: Colors.grey.shade300,
+              child: IconButton(
+                icon: const Icon(Icons.star),
+                onPressed: null,
+                disabledColor: Theme.of(context).iconTheme.color,
+              ),
+            ),
+            loadSuccess: (state) {
+              return IconButton(
+                icon: Icon(
+                  !state.repoDetail.isFresh
+                      ? MdiIcons.starRemoveOutline
+                      : state.repoDetail.entity?.starred == true
+                          ? Icons.star
+                          : Icons.star_outline,
+                ),
+                onPressed: !state.repoDetail.isFresh
+                    ? null
+                    : () {
+                        ref
+                            .read(repoDetailNotifierProvider.notifier)
+                            .switchStarredStatus(state.repoDetail.entity!);
+                      },
+              );
+            },
+          ),
+        ],
       ),
+      body: Container(),
     );
   }
 }
