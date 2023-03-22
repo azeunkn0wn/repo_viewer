258ffc1c12d9b6882c9e40d57141ac5b4248067e
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-22 17:22:05 +0800

    feat config reader

---
+++ lib/config_reader.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+abstract class ConfigReader {
+  static late Map<String, dynamic> _config;
+
+  static Future<void> initialize() async {
+    final configString = await rootBundle.loadString('config/app_config.json');
+    _config = json.decode(configString) as Map<String, dynamic>;
+  }
+
+  static String getEnv() {
+    return _config['env'] as String;
+  }
+}
