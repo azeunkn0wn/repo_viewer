2895235d0fbe9ceed855ba30f4dbe0b917f5e0c6
azeunkn0wn <azeunkn0wn@gmail.com>
2023-04-16 17:36:35 +0800

    refactor

--- test/core/presentation/app_widget_test.dart
+++ test/core/presentation/app_widget_test.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/annotations.dart';
@@ -17,62 +18,71 @@ import '../infrastructure/test_database.dart';
 @GenerateNiceMocks([MockSpec<GithubAuthenticator>()])
 void main() {
   late MockGithubAuthenticator mockGithubAuthenticator;
+
   setUp(() {
     mockGithubAuthenticator = MockGithubAuthenticator();
   });
 
-  Future<void> pumpAppWidget(WidgetTester tester) async {
-    await tester.pumpWidget(
-      ProviderScope(
-        overrides: [
-          githubAuthenticatorProvider.overrideWith(
-            (ref) => mockGithubAuthenticator,
-          ),
-          sembastProvider.overrideWith((ref) => TestSembastDatabase()),
-          initializationProvider.overrideWith((ref) async {
-            await ref.read(sembastProvider).init();
-            final authNotifier = ref.read(authNotifierProvider.notifier);
-            await authNotifier.checkAndUpdateAuthStatus();
+  Widget createWidgetUnderTest() {
+    return ProviderScope(
+      overrides: [
+        githubAuthenticatorProvider.overrideWith(
+          (ref) => mockGithubAuthenticator,
+        ),
+        sembastProvider.overrideWith((ref) => TestSembastDatabase()),
+        initializationProvider.overrideWith((ref) async {
+          await ref.read(sembastProvider).init();
+          final authNotifier = ref.read(authNotifierProvider.notifier);
+          await authNotifier.checkAndUpdateAuthStatus();
 
-            return unit;
-          }),
-        ],
-        child: AppWidget(),
-      ),
+          return unit;
+        }),
+      ],
+      child: AppWidget(),
     );
   }
 
-  testWidgets('should push to sign in page when user is signed out',
+  testWidgets('should push to sign-in page when user is not signed in',
       (tester) async {
+    // arrange
     when(mockGithubAuthenticator.isSignedIn()).thenAnswer(
       (_) => Future.delayed(
         const Duration(seconds: 1),
         () => false,
       ),
     );
-    await pumpAppWidget(tester);
-
+    // act
+    await tester.pumpWidget(createWidgetUnderTest());
+    // assert
     await tester.pump();
     expect(find.byType(SplashPage), findsOneWidget);
+    expect(find.byType(LinearProgressIndicator), findsOneWidget);
     await tester.pumpAndSettle();
     expect(find.byType(SplashPage), findsNothing);
+    expect(find.byType(StarredReposPage), findsNothing);
     expect(find.byType(SignInPage), findsOneWidget);
   });
 
   testWidgets('should push to sign in page when user is signed in',
       (tester) async {
+    // arrange
     when(mockGithubAuthenticator.isSignedIn()).thenAnswer(
       (_) => Future.delayed(
         const Duration(seconds: 1),
         () => true,
       ),
     );
-    await pumpAppWidget(tester);
 
+    // act
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    // assert
     await tester.pump();
     expect(find.byType(SplashPage), findsOneWidget);
+    expect(find.byType(LinearProgressIndicator), findsOneWidget);
     await tester.pumpAndSettle();
     expect(find.byType(SplashPage), findsNothing);
+    expect(find.byType(SignInPage), findsNothing);
     expect(find.byType(StarredReposPage), findsOneWidget);
   });
 }
